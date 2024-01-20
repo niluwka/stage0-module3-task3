47ffb1111b3aade464bb56c386e378c5066929c8
niluwka0403 <ninimamatova0403@gmail.com>
2024-01-20 20:30:57 +0500

    variable

--- src/main/java/school/mjc/stage0/module3/task3/DeclaringVars.java
+++ src/main/java/school/mjc/stage0/module3/task3/DeclaringVars.java
@@ -2,6 +2,13 @@ package school.mjc.stage0.module3.task3;
 
 public class DeclaringVars {
     public static void main(String[] args) {
-        //write code here
+        int a, b, c;
+        int x = 10;
+        a = x;
+        b = x;
+        c = x;
+
+        System.out.println(c);
+
     }
 }
--- src/main/java/school/mjc/stage0/module3/task3/ReassigningValues.java
+++ src/main/java/school/mjc/stage0/module3/task3/ReassigningValues.java
@@ -2,6 +2,18 @@ package school.mjc.stage0.module3.task3;
 
 public class ReassigningValues {
     public static void main(String[] args) {
-        //write code here
+        int first = 1;
+        int second = 10;
+        int third = 100;
+        System.out.println(first +"\n" + second +"\n" + third );
+       int linkToFirst = first;
+       int linkToSecond = second;
+       int linkToThird = third;
+       linkToFirst = 15;
+       linkToSecond = 6;
+       linkToThird = 4;
+        System.out.println(linkToFirst +"\n" + linkToSecond +"\n" + linkToThird );
+
+
     }
 }
